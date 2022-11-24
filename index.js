f017ebe97c263e3d2521d8ce709288135ac9050c
ScottShakaMwangi <scottshakamwangi@gmail.com>
2022-11-24 14:54:23 +0300

    added new files

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const mongoose = require("mongoose")
 const dotenv =require("dotenv");
 const userRoute = require("./routes/user")
 
-
-
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO_URL)
     .then(()=>console.log("Db connection succeful"))
-    .catch((err)=>{console.log(err);
+    .catch((err)=>{
+        console.log(err);
     });
 
+app.use(express.json());
+ app.use("/api/users", userRoute);
 
- app.use("/api/user", userRoute);
- 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log ("Backend sever is running!");
 });
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,12 @@ router.get("/usertest", (req,res)=>{
     res.send("user test is succeful");
 });
 
+router.post("/userposttest", (req,res)=>{
+    const username = req.body.username
+    res.send("your username is:" + username);
+})
+
+
 
 
 module.exports = router
